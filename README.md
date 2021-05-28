96d0d7566a6036de5960985c88f07bf003ed171f
Ammar Ahmed <35519796+ardehnus@users.noreply.github.com>
2021-05-28 05:21:07 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Football Club Logo Detection and Classification
+
+## Dataset:
+The dataset consists of a total of 300 images. 50 images per class.
+The classes are:
+
+1. Barcelona
+2. Real Madrid
+3. Manchester United
+4. Borussia Dortmund
+5. Inter Milan
+6. Chelsea
+
+Images souce: Google
+
+## Data processing:
+The model takes image of a fixed dimension. The image_processing.py file resizes each image to 224X224 dimensions. It converts the dataset to a numpy array and saves it in data.npy file. It also generates a labels.npy file consisting of target labels for each image in the dataset.
+
+## Model:
+The model makes use of the MobileNet_v2 architecture trained on the ImageNet dataset. The MobileNet_v2 model is smaller compared to other models. This helps in ease of training and the model can be used in android applications due to its relatively smaller size. The Dense layer of the model is replaced by our custom dense layer to classify 6 classes. The logo_classifier.ipynb generates a model.h5 file which cotains the trained model.
+
+
+## Prediction:
+The predictor.py file takes in images of club logos as input, resizes it to 224X224 to feed it to the trained model and outputs the target logo present in the image. The extra_images folder consists of some additional images for testing the model. 
---
+++ image_Preprocessing.py
@@ -0,0 +1,71 @@
+import os
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io as io
+import pandas as pd
+import random
+import glob
+
+path = os.getcwd()
+os.chdir(path + '/images')
+path = os.getcwd()
+
+def resize():
+    width, height = 224, 224  
+    for each in os.listdir(path):
+        print(each)
+        raw_image = Image.open(each)        
+        compressed_image = raw_image.resize((width, height), Image.ANTIALIAS)
+        compressed_image.save(each, quality = 95)
+
+def save_data(data):
+    np.save('data.npy', data)
+
+def load_data():
+    data = np.load('data.npy')
+    return data
+
+def convert_gray():
+    c = 0
+    for each in os.listdir(path):
+        img = Image.open(each).convert('1')
+        img.save(each)
+        c += 1
+
+def image_to_array():
+    count = 0
+    temp = []
+    labels = []
+    for each in os.listdir(path):
+        count += 1
+        temp.append(io.imread(each)) 
+        n = int(each[0:-4])          
+        if n < 50:  #Barca
+            labels.append(0)    
+        elif n >= 50 and n < 100:
+            labels.append(1)    #RMA
+        elif n >= 100 and n < 150:
+            labels.append(2)    #ManU
+        elif n >= 150 and n < 200:
+            labels.append(3)    #BVB
+        elif n >= 200 and n < 250:
+            labels.append(4)    #Inter
+        else:
+            labels.append(5)    #Chelsea
+        print(n, labels[-1])
+    arr = np.array(temp)
+    print('Total images: ', count)
+    print('Total labels: ', len(labels))
+    return arr, labels
+    
+def create_labels(labels):
+    labels_array = np.array(labels)
+    np.save('labels', labels_array)
+
+resize()
+data, labels = image_to_array()
+data = (data.astype(np.float32) - 127.5)/ 127.5
+data = data.reshape(data.shape[0], 224, 224, 3)
+save_data(data)
+create_labels(labels)
